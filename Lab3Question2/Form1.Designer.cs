018e67b0cbeb38db18f6a270249abc7f8561eb39
LOTIRELK <lorcan.james.otoole@gmail.com>
2017-06-07 12:57:39 +0100

    #]
    #]

---
+++ Lab3Question2/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Lab3Question2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAttempt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(75, 153);
+            this.btnGuess.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(138, 22);
+            this.btnGuess.TabIndex = 1;
+            this.btnGuess.Text = "G&uess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(385, 153);
+            this.btnQuit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(138, 22);
+            this.btnQuit.TabIndex = 2;
+            this.btnQuit.Text = "Q&uit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(114, 27);
+            this.txtNumber.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 27);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Attempts left:";
+            // 
+            // txtAttempt
+            // 
+            this.txtAttempt.Location = new System.Drawing.Point(424, 27);
+            this.txtAttempt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAttempt.Name = "txtAttempt";
+            this.txtAttempt.ReadOnly = true;
+            this.txtAttempt.Size = new System.Drawing.Size(100, 20);
+            this.txtAttempt.TabIndex = 5;
+            this.txtAttempt.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 83);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Position:";
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.Location = new System.Drawing.Point(114, 83);
+            this.txtPosition.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.ReadOnly = true;
+            this.txtPosition.Size = new System.Drawing.Size(100, 20);
+            this.txtPosition.TabIndex = 7;
+            this.txtPosition.TabStop = false;
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(424, 81);
+            this.txtAnswer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ReadOnly = true;
+            this.txtAnswer.Size = new System.Drawing.Size(100, 20);
+            this.txtAnswer.TabIndex = 8;
+            this.txtAnswer.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(288, 83);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Answer:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 210);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.txtPosition);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAttempt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnGuess);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Number Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAttempt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPosition;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab3Question2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//a progam that creates a random number and allows the user 3 attemmpts to guess
+//it correctly
+namespace Lab3Question2
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            //random number initialised here to stop it being changed each time
+            //calculate button is clicked
+            Random rand = new Random();
+            number = rand.Next(1,11);
+        }
+
+        int attempt = 3;
+        int number = 0;
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            //call function to compare the number entered by user and the random number
+            compareNumber();      
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            //close form when quit button clicked
+            this.Close();
+        }
+
+        //declaration of the function to compare the number entered with the random number
+        void compareNumber()
+        {
+            //convert tring to int
+            int guess = Convert.ToInt32(txtNumber.Text);
+            //check if number is not within the range
+            if (guess > 10 || guess < 1)
+            {
+                MessageBox.Show(
+                    "Please enter a whole number between one and ten",
+                    "Error!");
+                this.Close();//close form
+            }
+            if (guess > number)
+            {
+                txtPosition.Text = "Greater than.";
+            }
+            else if (guess < number)
+            {
+                txtPosition.Text = "Less than.";
+            }
+            else if (guess == number)
+            {
+                txtPosition.Text = "Equal to";
+                txtAnswer.Text = number.ToString();
+                
+            }
+
+            //minus attempt by one each time answer is incorrect
+            attempt--;
+
+            //output attempts left
+            txtAttempt.Text = attempt.ToString();
+
+            //when attempts are equal to 0
+            if (attempt == 0&&guess!=number)
+            {
+                //reveal answer
+                txtAnswer.Text = number.ToString();
+                MessageBox.Show("The correct number is " + 
+                    number.ToString()+". Please try again.");
+                this.Close();
+            }
+        }
+    }
+}
+
+
